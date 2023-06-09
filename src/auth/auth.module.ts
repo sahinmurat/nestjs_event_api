baee5d61b7b055a4a5c5c232810cd4f1e6f6992e
Murat Sahin <murat.sahin@inevvo-solutions.com>
2023-06-09 16:20:18 +0200

    jwt is implemented

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,11 +4,19 @@ import { User } from './user.entity';
 import { LocalStrategy } from './local.strategy';
 import { AuthController } from './auth.controller';
 import { Attendee } from '../events/attendee.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthService } from './auth.service';
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([User, Attendee])],
+        TypeOrmModule.forFeature([User, Attendee]),
+        JwtModule.registerAsync({
+            useFactory: () => ({
+                secret: process.env.JWT_SECRET,
+                signOptions: { expiresIn: '60m' }
+            })
+        })],
     controllers: [AuthController],
-    providers: [LocalStrategy],
+    providers: [LocalStrategy, AuthService],
 })
 export class AuthModule { }
