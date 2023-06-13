f6dc352de2745b3df54e13ebb3f889d9377f4d81
Murat Sahin <murat.sahin@inevvo-solutions.com>
2023-06-13 10:12:00 +0200

    password hashing with bvyrpt is implemented

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -4,6 +4,7 @@ import { Strategy } from "passport-local";
 import { Repository } from "typeorm";
 import { User } from "./user.entity";
 import { InjectRepository } from "@nestjs/typeorm";
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
@@ -28,7 +29,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
             throw new UnauthorizedException();
         }
 
-        if (password !== user.password) {
+        if (await bcrypt.compare(password, user.password) === false) {
             this.logger.debug(`Password ${password} is invalid`);
             throw new UnauthorizedException();
         }
